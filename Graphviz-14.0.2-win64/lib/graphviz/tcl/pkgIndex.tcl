19b62b835975528b744d382911a002f105e142e8
pulio12 <pulindha@gmail.com>
2025-11-16 16:20:19 +0100

    removing shit

--- Graphviz-14.0.2-win64/lib/graphviz/tcl/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-package ifneeded Tcldot 14.0.2 "
-	load [file join $dir libtcldot.dll] Tcldot"
-package ifneeded Tclpathplan 14.0.2 "
-	load [file join $dir libtclplan.dll] Tclpathplan"
-package ifneeded Gdtclft 14.0.2 "
-	load [file join $dir libgdtclft.dll] Gdtclft"
-# end
